c6dae91219e7d200044081cbad25d78597c8e7ce
Jtotheazzy <jahuang7@s.sfusd.edu>
2023-12-20 12:43:00 -0800

    player scene complete

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var Speed = 400
+
+@onready var bgmusic = $"../BGmusic"
+@onready var enemy_hit = $"../EnemyHit"
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -=1
+	if velocity.length() > 0: 
+		velocity = velocity.normalized()*Speed
+	position += velocity * delta
+
+func _on_body_entered(body):
+	enemy_hit.play()
+	bgmusic.stop()
+	hide()
